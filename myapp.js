5dd5a0f30586971e39c13c2044da9abab14a6dfe
Matthew Thees <matthewthees@Matthews-MBP-7.fios-router.home>
2017-02-07 17:13:13 -0500

    Still stuck, trying to simplify

--- myapp.js
+++ myapp.js
@@ -14,55 +14,40 @@
 //     $('#search-container').html('<pre>' + str + '</pre>');
 //   });
 // }
+
 var openWeatherMapID = 'e644ec33c271ca52c7361360d42d44b7'
 var cityID = '4138106'
-
-var openWeatherMapData;
-$.getJSON('http://api.openweathermap.org/data/2.5/forecast/city?id='+cityID+'&APPID='+openWeatherMapID, function(json){
-    openWeatherMapData = json;
-    console.log(openWeatherMapData)
-});
-
 var ctx = $('#myChart');
 
-function createChart(ctx, openWeatherMapData) {
-    var chart = new Chart(ctx, {
-        type: 'line',
-        data: {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: 'Degrees \'F\'',
-                data: [12, 9, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)',
-                    'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)',
-                    'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)',
-                    'rgba(255, 159, 64, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(255,99,132,1)',
-                    'rgba(54, 162, 235, 1)',
-                    'rgba(255, 206, 86, 1)',
-                    'rgba(75, 192, 192, 1)',
-                    'rgba(153, 102, 255, 1)',
-                    'rgba(255, 159, 64, 1)'
-                ],
-                borderWidth: 1
-            }]
-        },
-        options: {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero:true
-                    }
-                }]
-            }
-        }
+function getData(openWeatherMapID, cityID, ctx) {
+    var openWeatherMapData;
+    $.getJSON('http://api.openweathermap.org/data/2.5/forecast/city?id='+cityID+'&APPID='+openWeatherMapID, function(json){
+        openWeatherMapData = json;
+        console.log(openWeatherMapData)
+        createChart(ctx, openWeatherMapData)
     });
-    // cant seem to be able to pass in or edit data for this instantiation of the chart...
-    console.log(openWeatherMapData)
 }
-createChart(ctx, openWeatherMapData);
+
+function createChart(ctx, openWeatherMapData) {
+
+    var chart = new Chart (ctx, {
+        type: 'line',
+        data: { 
+
+        },
+        options: { scales: { yAxes: [{ ticks: { beginAtZero:true } }] } } 
+    })
+}
+
+getData(openWeatherMapID, cityID, ctx)
+
+// TESTER DATA
+//==============================================
+// labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+//             datasets: [{
+//                 label: 'Degrees \'F\'',
+//                 data: [12, 9, 3, 5, 2, 3],
+//                 backgroundColor: ['rgba(255, 99, 132, 0.2)'],
+//                 borderColor: ['rgba(255,99,132,1)'],
+//                 borderWidth: 1
+//             }]
